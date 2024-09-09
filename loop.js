df8843370179a3e2909a73d7adaa69ff908074cd
mahdi786islam <mahdiislam786@gmail.com>
2024-09-10 00:45:13 +0600

    second commit

---
+++ loop.js
@@ -0,0 +1,18 @@
+let i = 81;
+let total = 0;
+let sumOdd = 0;
+while( i <= 131 ){
+
+    
+
+    if( i % 2 === 1 ){
+
+        sumOdd = sumOdd + i;
+        console.log('sumOdd:', sumOdd)
+        
+    }
+
+    
+    i++;
+
+}
