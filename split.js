aa19ba4bc781a7cf9a155d21d7efb593c521ab59
mahdi786islam <mahdiislam786@gmail.com>
2024-09-10 14:43:29 +0600

    fifth commit

---
+++ split.js
@@ -0,0 +1,12 @@
+const sentence = 'i am a food person and eating individual' ;
+console.log(sentence.split('a'));
+const friends = 'rakib,jahid,karim,afil,jafil'
+console.log(friends.split(','));
+
+const realFriends = [ 'rakib', 'jahid', 'karim', 'afil', 'jafil' ];
+console.log(realFriends.join("|"));
+
+const string = 'mahdi islam';
+console.log(string.toUpperCase())
+const stringTwo = 'MAHDI ISLAM';
+console.log(stringTwo.toLowerCase())
